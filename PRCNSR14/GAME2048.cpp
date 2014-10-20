02c7024001df9a7e1134cdf9101773606c05f8f7
AshishKhatkar <akhatkar64@gmail.com>
2014-10-20 20:52:10 +0530

    Create GAME2048.cpp

---
+++ PRCNSR14/GAME2048.cpp
@@ -0,0 +1,20 @@
+/*
+ID: ashish1610
+PROG: GAME2048
+LANG: C++
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define ll	long long int
+int main()
+{
+	int t;
+	scanf("%d",&t);
+	while(t--)
+	{
+		ll n;
+		scanf("%lld",&n);
+		printf("%lld\n",n-1);
+	}
+	return 0;
+}
