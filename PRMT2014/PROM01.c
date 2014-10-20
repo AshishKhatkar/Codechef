44abad692ffff5518c6a83430ab004afa726b0f6
AshishKhatkar <akhatkar64@gmail.com>
2014-10-20 21:16:28 +0530

    Create PROM01.c

---
+++ PRMT2014/PROM01.c
@@ -0,0 +1,20 @@
+/*	ashish1610	*/
+#include<stdio.h>
+unsigned long long int n, m;
+int main()
+{
+	while(1)
+	{
+		scanf("%llu %llu",&n,&m);
+		if(n==0 && m==0) break;
+		if(n > m)
+		{
+			printf("%llu\n",(10*m*m*m+2*m)/3-4*m*m+((((m*5)-3)*(n-m))+((n-m)-1)*(n-m))*m);
+		}
+		else
+		{
+			printf("%llu\n",(10*n*n*n+2*n)/3-4*n*n+((((n*5)-3)*(m-n))+((m-n)-1)*(m-n))*n);
+		}
+	}
+	return 0;
+}
